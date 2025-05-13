8220fe8ea67a343311a61e6892cd3a07a76f07d8
Manohar saini <0xshadowww@gmail.com>
2025-05-13 23:21:57 +0530

    little bit adjustments

--- skill-testing/Swap.py
+++ skill-testing/Swap.py
@@ -2,9 +2,11 @@
 # today song - Pretty little baby
 # this is word swapper
 
+#inputs
 Word = input("what word you wanna reverse: ")
 Times = int(input("How many times you wanna print it: "))
 
+#loops
 for i in range(Times):
     print(Word)
 print("Reversed: ", Word[::-1]);
