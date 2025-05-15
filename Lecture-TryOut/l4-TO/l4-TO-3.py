c03c2b7a23ec00b5ea42968aefe3d807f8cf491a
Manohar saini <0xshadowww@gmail.com>
2025-05-15 09:34:31 +0530

    this have only  3 Lecture TryOut so what we can do

---
+++ Lecture-TryOut/l4-TO/l4-TO-3.py
@@ -0,0 +1,5 @@
+SecretNum = int(input("Give me Your Secret Number 1 - 100: "));
+for i in range(1 , 101):
+    if i == SecretNum:
+        print("Found it, is it: " , SecretNum);
+    
---
+++ Lecture-TryOut/l4-TO/l4-TO-4.py
@@ -0,0 +1,2 @@
+# In this lecture there are only 3 tryouts idk why.
+# but what can we do. lets code exercise
